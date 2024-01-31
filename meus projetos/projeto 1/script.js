3c3eb96fb7c7c7948529dec425f6f8cd58649102
Davidproje <158210395+Davidproje@users.noreply.github.com>
2024-01-30 21:04:11 -0300

    Pessoa mais especial do mundo

---
+++ meus projetos/projeto 1/script.js
@@ -0,0 +1,11 @@
+var imgatual = "pexels-kawaiiart-1767434.jpg"
+var imganterior ="pexels-pixabay-326612.png"
+
+function alterar(){
+    document.getElementById('img').src="pexels-kawaiiart-1767434.jpg";
+
+    
+    
+
+    
+}
